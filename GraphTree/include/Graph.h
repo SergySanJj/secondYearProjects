0700788f044f2b9e0f68dba0d20b9a16bddb0633
Sergei Yarema <isara.isara8@gmail.com>
2018-09-21 18:38:03 +0300

    Save to start work on PC

--- GraphTree/include/Graph.h
+++ GraphTree/include/Graph.h
@@ -16,13 +16,17 @@
 
 namespace GraphTree {
 
-    enum Errors {BadAlloc=0};
+    enum Errors {
+        BadAlloc = 0
+    };
 
     // Graph node that contains some data.
-    template<typename T> class Vertex;
+    template<typename T>
+    class Vertex;
 
     // Structure to work with numerated Graphs.
-    template<typename T> class Graph;
+    template<typename T>
+    class Graph;
 
 
     template<typename T>
@@ -30,6 +34,7 @@ namespace GraphTree {
 
     public:
         Vertex<T>() {}
+
         explicit Vertex<T>(T data) : vertexData(data) {}
 
         ~Vertex<T>() {}
@@ -46,17 +51,18 @@ namespace GraphTree {
     public:
 
         // Create an empty graph with n verticies.
-        Graph(std::size_t n=0);
+        Graph(std::size_t n = 0);
 
         ~Graph();
 
         // TODO: write copy constructor, assigment operator
 
         // Call as .addVertex(Vertex(data));
-        void addVertex(const Vertex<T>& v);
+        void addVertex(const Vertex<T> &v);
 
-        void addEdge(const Vertex<T>& u, const Vertex<T>& v);
-        void addOrientedEdge(const Vertex<T>& u, const Vertex<T>& v);
+        void addEdge(const Vertex<T> &u, const Vertex<T> &v);
+
+        void addOrientedEdge(const Vertex<T> &u, const Vertex<T> &v);
 
         std::shared_ptr<Graph<T>> getSpanningTree();
 
@@ -66,9 +72,9 @@ namespace GraphTree {
         // Vertex number.
         std::size_t N;
         // Here will be stored pointers to verts.
-        std::vector< std::shared_ptr< Vertex<T> > > vertexList;
+        std::vector<std::shared_ptr<Vertex<T> > > vertexList;
         // Adjacency list.
-        std::vector< std::set< std::size_t > > adjList;  // TODO: think once more about using hash map insted
+        std::vector<std::set<std::size_t> > adjList;  // TODO: think once more about using hash map insted
     };
 
 /********--------Implementation--------********/
@@ -76,28 +82,17 @@ namespace GraphTree {
     template<typename T>
     Graph<T>::Graph(std::size_t n): N(n) {
 
-        try
-        {
-            vertexList.resize(N);
-            adjList.resize(N);
 
-        }
-        catch (std::bad_alloc& ba)
-        {
-            std::cerr << "bad_alloc caught: " << ba.what() << '\n';
-            throw Errors::BadAlloc;
-        }
+        vertexList.resize(N);
+        adjList.resize(N);
 
-        for (auto vert = vertexList.begin(); vert != vertexList.end(); vert++)
-        {
+
+        for (auto vert = vertexList.begin(); vert != vertexList.end(); vert++) {
             vert(new Vertex<T>());
         }
     }
 
 }
-
-
-
 
 
 #endif //GRAPHTREE_GRAPH_H
