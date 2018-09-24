6aefb25444e143898e180c2d8ce2dafff43c5e0f
SergeiSanJj <isara.isara8@gmail.com>
2018-09-24 20:19:57 +0300

    Prepare for renaming re profiling repo

--- Tst/CMakeFiles/Grid2D.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Grid2D.
-# This may be replaced when dependencies are built.
--- Tst/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "VFMatrix.h"
-
-int main(int, char**)
-{
-    VFMatrix m(3);
-    std::cout << "Hello, wo!\n";
-    std::cin.get();
-}
